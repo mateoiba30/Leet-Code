3000521b046741858aa7f7a83c21220984ef0fe1
Mateo Ibanez <mateoiba30@gmail.com>
2024-06-22 16:53:23 -0300

    agrego ejercicio 38

--- 09-binary-tree/ex38-lowes-ancestor.py
+++ 09-binary-tree/ex38-lowes-ancestor.py
@@ -3,3 +3,26 @@
 
 #exercise 236
 
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left: Optional['TreeNode'] = None, right: Optional['TreeNode'] = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+  def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    if not root or root == p or root == q:
+      return root #if one of the nodes we are looking for is de root, it means he is the lowest common ancestor
+
+    left = self.lowestCommonAncestor(root.left, p, q)
+    right = self.lowestCommonAncestor(root.right, p, q)
+
+    if left!=None and right!=None: #if we founded p and q in different subtrees of the actual root
+      return root
+    
+    if left != None:
+       return left
+    return right #maybe is None
+  
+#we explore the tree, if a recursive path from a node ends without finding p or q, the node it will return None to his father. On the other hand, if there was p or q on the path, the node it will return itself. This all means that when we find a node where boths children returned Not None is because they both have p and q respectively, beeing the actual node the answer
