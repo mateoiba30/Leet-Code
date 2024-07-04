3cc1634576a716d952a0f6404bb60a8eb4b7f331
Mateo Ibanez <mateoiba30@gmail.com>
2024-07-04 15:40:09 -0300

    corrijo ej 39 y hago ej 40

--- 10-binary-tree-bfs/ex40-max-level-sum.py
+++ 10-binary-tree-bfs/ex40-max-level-sum.py
@@ -1,10 +1,12 @@
-#Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+#Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+#Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
 
-#exercise 199
+#exercise 1161
 
-from typing import Optional
 from typing import List
-from collections import deque
+from typing import Optional
+from typing import deque
+import math
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -13,38 +15,33 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        right_view = []
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        max_sum = -math.inf
+        best_level = 0
+        actLevel = 1
+        
         if root:
-            right_view.append(root.val)
             list = deque()
             list.append(root)
             list.append(None) #marks the end of each level
+            level_sum = 0
 
-            while list:
+            while list:   
                 actNode = list.popleft()
                 if actNode:
+                    level_sum += actNode.val
                     if actNode.left:
                         list.append(actNode.left)
                     if actNode.right:
                         list.append(actNode.right)
                 else:
+                    if level_sum > max_sum:
+                        max_sum = level_sum
+                        best_level = actLevel
+                    level_sum = 0
+                    actLevel += 1
                     if list: 
-                        right_view.append(list[-1].val)
                         list.append(None) #marks the end of each level, this means that now the first node is the rightest of his level
 
-        return right_view
+        return best_level
     
-#        1
-#         \
-#          3
-
-root = TreeNode(1)
-root.right = TreeNode(3)
-
-# Crear una instancia de Solution y llamar a rightSideView
-solution = Solution()
-result = solution.rightSideView(root)
-
-# Imprimir el resultado
-print("Right side view of the tree:", result)
